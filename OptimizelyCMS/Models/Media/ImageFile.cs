2f5895465c0bc4ff8b079bb3ad397ab653446633
Erik Henningson <ErikHen@users.noreply.github.com>
2021-10-20 11:06:23 +0200

    Add imagepoint editor to Optimizely sample

--- OptimizelyCMS/Models/Media/ImageFile.cs
+++ OptimizelyCMS/Models/Media/ImageFile.cs
@@ -3,7 +3,7 @@ using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
 using EPiServer.Framework.DataAnnotations;
-//using ImagePointEditor;
+using ImagePointEditor;
 
 namespace OptimizelyCMS.Models.Media
 {
@@ -14,7 +14,7 @@ namespace OptimizelyCMS.Models.Media
         [Display(Name = "Alt text", Order = 10)]
         public virtual string AltText { get; set; }
 
-  //      [UIHint(ImagePoint.UIHint)]
+        [UIHint(ImagePoint.UIHint)]
         [Display(Name = "Focal point")]
         public virtual string ImageFocalPoint { get; set; }
     }
